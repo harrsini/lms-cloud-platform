5bc4bd5d7e8bef357bcfa5595e38af478544b5a6
harrsini <harrsinims@gmail.com>
2026-03-05 04:23:11 +0530

    Switch cloud deployment to SQLite

--- cse_portal/settings.py
+++ cse_portal/settings.py
@@ -82,13 +82,9 @@ WSGI_APPLICATION = 'cse_portal.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'cse_portal_db',
-        'USER': 'postgres',
-        'PASSWORD': 'harrs123',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
